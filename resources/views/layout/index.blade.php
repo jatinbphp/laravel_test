fbb3e54be3866166d1dcc17c0e254947db0cbd5e
Bhavesh Vyas <bhavesh.v.php@gmail.com>
2019-08-08 14:02:22 +0530

    commit the till now data cahnge and push

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -8,20 +8,20 @@
         </title>
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport"/>
         <meta content="{{ csrf_token() }}" name="_token"/>
-        {{ Html::style("assets/bootstrap/css/bootstrap.min.css") }}
-        {{ Html::style('assets/plugins/datatables/dataTables.bootstrap.min.css') }}
-        {{ Html::style("assets/plugins/font-awesome-4.7.0/css/font-awesome.min.css") }}
-        {{ Html::style("assets/dist/css/ionicons.min.css") }}
-        {{ Html::style('assets/plugins/select2/select2.min.css') }}
-        {{ Html::style("assets/dist/css/AdminLTE.min.css") }}
-        {{ Html::style("assets/dist/css/skins/skin-blue.min.css") }}
-        {{ Html::style('assets/plugins/swal/swal.min.css') }}
-        {{ Html::style('assets/plugins/colorbox/colorbox.min.css') }}
-        {{ Html::style('assets/plugins/jstree/dist/themes/default/style.min.css') }}
-        {{ Html::style('assets/plugins/daterangepicker/daterangepicker-bs3.css') }}
-        {{ Html::style('assets/plugins/datetimepicker-master/jquery.datetimepicker.css') }}
-        {{ Html::style('assets/plugins/daterangepicker/daterangepicker-bs3.css') }}
-        {{ Html::style('assets/dist/css/test.css') }}
+        {{ Html::style("public/assets/bootstrap/css/bootstrap.min.css") }}
+        {{ Html::style('public/assets/plugins/datatables/dataTables.bootstrap.min.css') }}
+        {{ Html::style("public/assets/plugins/font-awesome-4.7.0/css/font-awesome.min.css") }}
+        {{ Html::style("public/assets/dist/css/ionicons.min.css") }}
+        {{ Html::style('public/assets/plugins/select2/select2.min.css') }}
+        {{ Html::style("public/assets/dist/css/AdminLTE.min.css") }}
+        {{ Html::style("public/assets/dist/css/skins/skin-blue.min.css") }}
+        {{ Html::style('public/assets/plugins/swal/swal.min.css') }}
+        {{ Html::style('public/assets/plugins/colorbox/colorbox.min.css') }}
+        {{ Html::style('public/assets/plugins/jstree/dist/themes/default/style.min.css') }}
+        {{ Html::style('public/assets/plugins/daterangepicker/daterangepicker-bs3.css') }}
+        {{ Html::style('public/assets/plugins/datetimepicker-master/jquery.datetimepicker.css') }}
+        {{ Html::style('public/assets/plugins/daterangepicker/daterangepicker-bs3.css') }}
+        {{ Html::style('public/assets/dist/css/test.css') }}
         @section("style")
         @show
     </head>
@@ -49,30 +49,30 @@
         <script>
             var public_path = "{!! URL::to('/'); !!}/";
         </script>
-        {{ Html::script("assets/plugins/jQuery/jquery-3.3.1.min.js") }}
-        {{ Html::script("assets/bootstrap/js/bootstrap.min.js")}}
-        {{ Html::script('assets/plugins/datatables/jquery.dataTables.js') }}
-        {{ Html::script('assets/plugins/datatables/dataTables.bootstrap.js') }}
-        {{ Html::script('assets/plugins/datatables/dataTables.bootstrap.min.js') }}
-        {{ Html::script('assets/plugins/datatables/dataTables.scrollingPagination.js') }}
-        {{ Html::script('assets/js/donetyping.js') }}
-        {{ Html::script('assets/plugins/colorbox/colorbox.min.js') }}
-        {{ Html::script('assets/js/fnStandingRedraw.js') }}
-        {{ Html::script("assets/js/jeditable.min.js")}}
-        {{ Html::script("assets/js/jquery.livequery.js")}}
-        {{ Html::script('assets/plugins/chartjs/Chart.min.js') }}
-        {{ Html::script('assets/plugins/swal/swal.min.js') }}
-        {{ Html::script("assets/dist/js/app.min.js")}}
-        {{ Html::script("assets/dist/js/demo.js")}}
-        {{ Html::script('assets/plugins/jstree/dist/jstree.min.js') }}
-        {{ Html::script('assets/plugins/daterangepicker/moment.js') }}
-        {{ Html::script('assets/js/jquery.form.min.js') }}
-        {{ Html::script('assets/plugins/datetimepicker-master/jquery.datetimepicker.full.js') }}
-        {{ Html::script('assets/plugins/daterangepicker/daterangepicker.js') }}
-        {{ Html::script('assets/plugins/ckeditor/ckeditor.js') }}
-        {{ Html::script('assets/plugins/fieldsaddmore/jqery.fieldsaddmore.min.js') }}
-        {{ Html::script('assets/plugins/select2/select2.full.js') }}
-        {{ Html::script("assets/dist/js/test.js") }}
+        {{ Html::script("public/assets/plugins/jQuery/jquery-3.3.1.min.js") }}
+        {{ Html::script("public/assets/bootstrap/js/bootstrap.min.js")}}
+        {{ Html::script('public/assets/plugins/datatables/jquery.dataTables.js') }}
+        {{ Html::script('public/assets/plugins/datatables/dataTables.bootstrap.js') }}
+        {{ Html::script('public/assets/plugins/datatables/dataTables.bootstrap.min.js') }}
+        {{ Html::script('public/assets/plugins/datatables/dataTables.scrollingPagination.js') }}
+        {{ Html::script('public/assets/js/donetyping.js') }}
+        {{ Html::script('public/assets/plugins/colorbox/colorbox.min.js') }}
+        {{ Html::script('public/assets/js/fnStandingRedraw.js') }}
+        {{ Html::script("public/assets/js/jeditable.min.js")}}
+        {{ Html::script("public/assets/js/jquery.livequery.js")}}
+        {{ Html::script('public/assets/plugins/chartjs/Chart.min.js') }}
+        {{ Html::script('public/assets/plugins/swal/swal.min.js') }}
+        {{ Html::script("public/assets/dist/js/app.min.js")}}
+        {{ Html::script("public/assets/dist/js/demo.js")}}
+        {{ Html::script('public/assets/plugins/jstree/dist/jstree.min.js') }}
+        {{ Html::script('public/assets/plugins/daterangepicker/moment.js') }}
+        {{ Html::script('public/assets/js/jquery.form.min.js') }}
+        {{ Html::script('public/assets/plugins/datetimepicker-master/jquery.datetimepicker.full.js') }}
+        {{ Html::script('public/assets/plugins/daterangepicker/daterangepicker.js') }}
+        {{ Html::script('public/assets/plugins/ckeditor/ckeditor.js') }}
+        {{ Html::script('public/assets/plugins/fieldsaddmore/jqery.fieldsaddmore.min.js') }}
+        {{ Html::script('public/assets/plugins/select2/select2.full.js') }}
+        {{ Html::script("public/assets/dist/js/test.js") }}
         @section('script')
         @show
     </body>
